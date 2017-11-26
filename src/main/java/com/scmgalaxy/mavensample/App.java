b7d3cf366588ff1a233427d3f399a1bdeaff5b89
dhamur <damodaram.gmr@gmail.com>
2017-11-26 15:09:54 +0530

    DEV-2
    DEV-2

--- src/main/java/com/scmgalaxy/mavensample/App.java
+++ src/main/java/com/scmgalaxy/mavensample/App.java
@@ -1,4 +1,4 @@
-package com.scmgalaxy.mavensample;
+package com.devopsfly.mavensample;
 
 /**
  * Hello world!
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World RefX! MAH-2--d" );
+        System.out.println( "Hello World RefX! DEV-2--d" );
     }
 }
